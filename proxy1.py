f88ca33c4bfa68b7c8293551430be2526d719be7
mil3zh <milena.lazzaretti5@gmail.com>
2021-02-12 18:16:43 +0100

    rotating proxy in scraper.py

--- proxy1.py
+++ proxy1.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 def proxy_generator():
   response = requests.get("https://sslproxies.org/")
   soup = BeautifulSoup(response.content, 'html5lib')
-  proxy = {'https': choice(list(zip(map(lambda x:x.text, soup.findAll('td')[::8]), map(lambda x:x.text, soup.findAll('td')[1::8]))))))}
+  proxy = {'https': choice(list(zip(map(lambda x:x.text, soup.findAll('td')[::8]), map(lambda x:x.text, soup.findAll('td')[1::8]))))}
 
   return proxy
 
@@ -23,18 +23,17 @@ def data_scraper(request_method, url, **kwargs):
     return response
 
 HEADER = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
-URL = "https://www.website.php"
+URL = "https://scholar.google.com/"
 PROXY = {"https": "https//59.110.7.190:1080"}
 
 response = requests.get(URL, proxies=PROXY, headers=HEADER)
 page_html = response.text
 page_soup = soup(page_html, "html.parser")
-
 adresses = page_soup.findAll("li", {"class":"list-group-item"}) #for example
 
 for address in adresses:
     try:
-        #parsing the html
+        print(address)
     except (TypeError):
         f.write("invalid data" + "\n")
 time.sleep(random.randint(1, 10))
