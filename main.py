e5184a8e59547bd7209f9f122c4b0cbf47483121
qwer_ty <qwer_ty@qwer_ty.com>
2021-02-21 15:10:52 +0100

    buggy but working

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import search_db.crossref as search_db_crossref
 
 #storing of empty DataFrame
 df_empty= pandas_utils.df_empty_creator()
-display(df_empty)
+# display(df_empty)
 
 # words to research without operators
 search_no_operators= "cannabis+depression"
@@ -12,8 +12,12 @@ search_no_operators= "cannabis+depression"
 search_with_operators= "cannabis AND depression"
 
 # Empty DataFrame feeders
-df_full_pubmed= search_db_pubmed.pubmed_df_feeder(df_empty, search_with_operators)
-display(df_full_pubmed)
+df_pubmed= search_db_pubmed.pubmed_df_feeder(df_empty, search_with_operators)
+# display(df_pubmed)
 
-df_full_crossref= search_db_crossref.crossref_df_feeder(df_empty, search_no_operators)
-display(df_full_crossref)
+df_crossref= search_db_crossref.crossref_df_feeder(df_empty, search_no_operators)
+# display(df_crossref)
+
+df_full= pandas_utils.df_full_merging(df_crossref, df_pubmed)
+# display(df_full)
+df_full.to_excel("./excels/df{}.xlsx".format(search_no_operators))
--- pandas_utils.py
+++ pandas_utils.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
-# create empty DataFrame
+# Create empty DataFrame
 def df_empty_creator():
     df= {'title':[], 'publication date':[],'subject':[], 'journal':[], 'DOI':[], 'author':[], 'abstract':[], 'from_database':[]}
     df= pd.DataFrame(data=df)
     df= df.astype(str)
-    # add following line directly to db_df_feeder
+    # Add following line directly to db_df_feeder (may be adapted)
     # df['publication date']= pd.to_datetime(df['publication date'])
     return(df)
+
+# Merge filled df from databases searching
+def df_full_merging(*argv):
+    for arg in argv:
+        merged_df= arg.append(arg, ignore_index = True)
+    return(merged_df)
