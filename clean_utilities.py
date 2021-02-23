38d234ebd66b8b54e79101e4e1a2937103329850
mil3zh <milena.lazzaretti5@gmail.com>
2021-02-23 20:26:20 +0100

    add stopwords filter

--- clean_utilities.py
+++ clean_utilities.py
@@ -1,54 +1,9 @@
 import re
-from bs4 import BeautifulSoup as bs
+from nltk.corpus import stopwords
+import pandas as pd
 
-def text(my_list):
-	clean_lst = []
-	clean_rgx = re.compile(r'\[.+\]')
-	for elem in my_list:
-		obj = elem.get_text()
-		obj = re.sub(clean_rgx, '', obj)
-		clean_lst.append(obj.strip())
-	return (clean_lst)
-
-def author(authors_list):
-	authors_lst = []
-	clean_rgx = re.compile(r'\s-.+')
-	for elem in authors_list:
-		obj = elem.get_text()
-		obj = re.sub(clean_rgx, '', obj)
-		authors_lst.append(obj.strip())
-	return (authors_lst)
-
-def publication_year(authors_list):
-	year_lst = []
-	extract_rgx = re.compile(r'\d{4}')
-	for elem in authors_list:
-		year = re.findall(extract_rgx, elem.get_text())
-		year_lst.append(''.join(year))
-	return (year_lst)
-
-def DOI(titles_list):
-	DOI_lst = []
-	doi_rgx = re.compile(r'.+/doi/abs')
-	for elem in titles_list:
-		try:
-			obj = elem.find('a')['href']
-			obj = re.sub(doi_rgx, 'https://doi.org', obj)
-		except TypeError:
-			obj = ""
-			#print("empty value")
-			pass
-		DOI_lst.append(obj.strip())
-	return (DOI_lst)
-
-def data(html):
-	titles = html.find_all('h3', class_="gs_rt")
-	authors = html.find_all(class_='gs_a')
-	abstracts = html.find_all(class_='gs_rs')
-	years = publication_year(authors)
-	doi = DOI(titles)
-	titles = text(titles)
-	authors = author(authors)
-	abstracts = text(abstracts)
-	data = list(zip(authors, titles, abstracts, years, doi))
-	return (data)
+def stop_words(original):
+	sw_list = set(stopwords.words('english')) # you need to download the corpus on your machine -> python3 -m nltk.downloader stopwords
+	new = original.applymap(lambda col: [x.lower() for x in str(col).split()])
+	new = new.applymap(lambda col: ' '.join([w for w in col if w not in sw_list]))
+	return (new)
