30a228efff1032e43f2220bad0402737192d7a14
mil3zh <milena.lazzaretti5@gmail.com>
2021-02-24 21:23:51 +0100

    add scores to df and sort

--- tf_idf.py
+++ tf_idf.py
@@ -3,8 +3,9 @@ import pandas as pd
 import string
 from collections import Counter
 import nlp_basics as nlp
-import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
 # TF-IDF term frequency - inverted document frequency
 # takes into account how many times a term appear and in how many docs
@@ -18,9 +19,16 @@ def text2Vector(texts):
 	return (vectors)
 
 def cosine_similarity(vector):
+    print("Computing similarity scores ...")
     return (vector * vector.T).toarray()
 
 def similarity_matrix(texts):
 	vectors = text2Vector(texts)
 	scores = cosine_similarity(vectors)
 	return (scores)
+
+def get_scores(rows, values):
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	scores = [np.sum(v) for v in values]
+	scores = np.array(scores).reshape(rows, -1)
+	return scaler.fit_transform(scores)
--- tf_idf_similarity.py
+++ tf_idf_similarity.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import nlp_basics as nlp
 import tf_idf  #term frequency vectorizer
+import numpy as np
 
 df = pd.read_csv('data.csv', delimiter=',', usecols=[1,2,3,4,5,6,7,8])
+df.drop_duplicates(inplace=True)
 # change title+abstract into a unique column of text
 dataset = df.filter(['title','abstract'], axis=1)
 dataset['text'] = df['title'].str.cat(df[['abstract']].astype(str), sep=" ")
@@ -11,6 +13,9 @@ dataset = nlp.remove_stopwords(dataset)
 # change dataset['text'] into a simple list
 texts = dataset['text'].tolist()
 vectors = tf_idf.similarity_matrix(texts)
+scores = tf_idf.get_scores(len(df),vectors)
+df['tfidf_score'] = scores
+df = df.sort_values(by=['tfidf_score'], ascending=False)
 
 #pd.set_option('display.max_colwidth', None)
-print(vectors)
+print(df)
