3735d1d1aa2f9afd83a25718f0ed79d18878b647
qwert_y <arlnpsytutanota.com>
2021-03-16 17:39:12 +0100

    working shit

--- search_db/frontiersin.py
+++ search_db/frontiersin.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import requests
 import sys # needed for import pandas_utils from parent folder
 sys.path.append("/..") # needed for import pandas_utils from parent folder
 import pandas_utils
+from datetime import datetime
+import time
+
 
 def get_browser():
     options = webdriver.ChromeOptions()
@@ -14,6 +17,7 @@ def get_browser():
     browser = webdriver.Chrome(options=options) #just changed chrome_options to options to clean warning
     return (browser)
 
+# ajouter function scroll down dans selenium
 
 df = pandas_utils.df_empty_creator()
 browser = get_browser()
@@ -23,7 +27,21 @@ keywords = "gifted+children+behavior+problems"
 
 page1 = browser.get("https://www.frontiersin.org/search?query={}&tab=articles".format(keywords))
 # test= browser.find_elements_by_xpath('//a[contains(@href,"altmetric")]')
+time.sleep(2)  # Allow 2 seconds for the web page to open
+scroll_pause_time = 1 # You can set your own pause time. My laptop is a bit slow so I use 1 sec
+screen_height = browser.execute_script("return window.screen.height;")   # get the screen height of the web
+i = 1
 
+while True:
+    # scroll one screen height each time
+    browser.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
+    i += 1
+    time.sleep(scroll_pause_time)
+    # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+    scroll_height = browser.execute_script("return document.body.scrollHeight;")
+    # Break the loop when the height we need to scroll to is larger than the total scroll height
+    if (screen_height) * i > scroll_height:
+        break
 
 # lnks= browser.find_elements_by_tag_name("a")
 # for lnk in lnks:
@@ -32,41 +50,32 @@ page1 = browser.get("https://www.frontiersin.org/search?query={}&tab=articles".f
 
 
 html1 = browser.page_source
-page1 = bs(html1, "html.parser")
+page1 = BeautifulSoup(html1, "html.parser")
 # browser.close()
-
 test2 = page1.find_all("a", href=True)
 list = []
 for t in test2:
     if 'altmetric' in t['href']:
         list.append(t['href'])
     #ajouter une regex pour ne garder que
-print(list)
 
 for url in list:
+    print(url)
+
+for count, url in enumerate(list):
     page2 = browser.get(url)
     html2 = browser.page_source # retrieve the page source of the webpage browser is accessing
-    page2 = bs(html2, "html.parser")
-    #OK print(page2)
-    ths = page2.find_all("th")
-    print(ths)
-    for th in ths:
-        if 'Title' in th:
-            print(th["Title"])
-    # print(page2)
-    # page2.type()
-    # title= page2.find_all("th", td=True)#title to df
-    # df['title']+= title
-    # test4= page2.find("td", text="Title")
-    # .find_next_sibling("td").text
-    # DOI
-    #abstarct
-    #published in
+    page2 = BeautifulSoup(html2, "html.parser")
+    try:
+        df.at[count, 'title'] = page2.find('td').findNext("div").get_text()
+        df.at[count, 'DOI'] = page2.find('td').findNext("a")['href']
+        df.at[count, 'abstract'] = page2.find("td").findNext("div").findNext("p").findNext("span").get_text() # not working for full text
+        df.at[count, 'abstract'] += page2.find("td").findNext("div").findNext("p").findNext("span").findNext("span").get_text()
+        df.at[count, 'publication date'] = page2.find("tbody").findNext("tr").findNext("tr").findNext("td").findNext("div").get_text()
+        df.at[count, 'publication date'].split(',') # keep date only year
+        # add reste to journal
+    except AttributeError:
+        pass
 
-# display(df)
-
-
-# print(test4)
-#on veut tous les trucs de l'id articles-results
-# chopper les liens altmetric et retrieve de là les infos
-# ajouter function scroll down dans selenium
+df['from_database']= 'frontiersin'
+df.to_csv("./excels/dfFRONTIERSTEST{}.csv".format(datetime.now().time()))
--- search_db/pyscopus.py
+++ search_db/pyscopus.py
@@ -11,22 +11,23 @@ sys.path.append(".")
 from apikeys import scopus_key
 
 def scopus_df_feeder(keywords):
-    df= pandas_utils.df_empty_creator() # create empty df
+    df = pandas_utils.df_empty_creator() # create empty df
 
 # say Hello to Scopus
-    limit= 2 # max tested = 2000
+    limit = 2 # max tested = 2000
     scopus = Scopus(scopus_key)
 
     search_df = scopus.search("KEY(keywords)", count=limit, view='STANDARD')
 
+# fill df empty df
     for i in search_df.index:
-        df['title']= search_df['title']
-        df['publication date']= search_df['cover_date']
-        df['journal']= search_df['publication_name']
-        df['DOI']= search_df['doi']
+        df['title'] = search_df['title']
+        df['publication date'] = search_df['cover_date']
+        df['journal'] = search_df['publication_name']
+        df['DOI'] = search_df['doi']
 
     # fill database column
-    df['from_database']= 'scopus'
+    df['from_database'] = 'scopus'
     # apply date time format to publication date column
-    df['publication date']= pd.to_datetime(df['publication date'],yearfirst='True')
+    df['publication date'] = pd.to_datetime(df['publication date'],yearfirst='True')
     return(df)
