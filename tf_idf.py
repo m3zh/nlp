dd58825ef3a9261998f3cc7eb8ea9e377324034e
y3zh <mil3zh@protonmail.com>
2021-03-21 09:30:56 +0100

    add word reweighting

--- tf_idf.py
+++ tf_idf.py
@@ -5,22 +5,42 @@ from collections import Counter
 import nlp_basics as nlp
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import MinMaxScaler
+from flashtext import KeywordProcessor
+import pickle
 import numpy as np
-
+from PyDictionary import PyDictionary
+from nltk.corpus import wordnet as wn
 # TF-IDF term frequency - inverted document frequency
 # takes into account how many times a term appear and in how many docs
 # if a term is not frequent (appears only once per doc) but it's present in all docs, its weigth is high
 # if a term appears frequently in a few texts but not in many others, its weigth is lowered down
 
+def reweighting(texts, keyword):
+    syn_dict = dict(pickle.load(open("synonyms.dict", "rb")))
+    if keyword not in syn_dict:
+        pydict = PyDictionary()
+        syn_dict[keyword] = pydict(keyword)
+    else:
+        print("Synonym already in dictionary as: "+str(syn_dict[keyword]))
+    synonym = { keyword : syn_dict[keyword] }
+    # print(wn.synset('talented.a.01').lemma_names()) --> get input from user
+    processor = KeywordProcessor()
+    processor.add_keywords_from_dict(synonym)
+    texts = [processor.replace_keywords(t) for t in texts]
+    texts = [t+' '+keyword if keyword in t else t for t in texts]
+    return (texts)
+
+
 # take a list of texts,
 # change them into vectors
 # and return a lists of vectors
 def text2Vector(texts):
-	print("Vectorizing ...")
-	# in tokenizer, you can use either stemTokenizer or lemmaTokenizer
-	tfidf_vectorizer = TfidfVectorizer(tokenizer=nlp.stemTokenizer) # , stop_words='english') <- we can omit this parameter, as we remove stopwords upfront
-	vectors = tfidf_vectorizer.fit_transform(texts)
-	return (vectors)
+    print("Vectorizing ...")
+    # in tokenizer, you can use either stemTokenizer or lemmaTokenizer
+    tfidf_vectorizer = TfidfVectorizer(tokenizer=nlp.stemTokenizer) # , stop_words='english') <- we can omit this parameter, as we remove stopwords upfront
+    texts = reweighting(texts, 'autism')
+    vectors = tfidf_vectorizer.fit_transform(texts)
+    return (vectors)
 
 # compute how close each vector is from the others
 def cosine_similarity(vector):
@@ -30,13 +50,13 @@ def cosine_similarity(vector):
 # returns a list of scores
 # based on the similarity of each text
 def similarity_matrix(texts):
-	vectors = text2Vector(texts)
-	scores = cosine_similarity(vectors)
-	return (scores)
+    vectors = text2Vector(texts)
+    scores = cosine_similarity(vectors)
+    return (scores)
 
 # normalize scores in a range from 0 to 1
 def get_scores(rows, values):
-	scaler = MinMaxScaler(feature_range=(0, 1))
-	scores = [np.sum(v) for v in values]
-	scores = np.array(scores).reshape(rows, -1) # to fit the df shape
-	return scaler.fit_transform(scores) # normalize 0, 1
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scores = [np.sum(v) for v in values]
+    scores = np.array(scores).reshape(rows, -1) # to fit the df shape
+    return scaler.fit_transform(scores) # normalize 0, 1
