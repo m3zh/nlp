72686cf90da237e048908c45b30072944a0c9eae
y3zh <mil3zh@protonmail.com>
2021-03-25 17:02:42 +0100

    comments + code clean up

--- search_db/google_scholar/search_utilities.py
+++ search_db/google_scholar/search_utilities.py
@@ -11,39 +11,30 @@ file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 import clean_utilities as clean
 import selenium_utils
-import search_utilities as s
 
-def selenium_search(keywords, browser, limit):
-	payload = (keywords).replace(' ', '+') # change parameters keyword in url form
-	df = [] # creates an empty dataframe
-	for i in range(0, limit, 10):
-		browser.get('https://scholar.google.com/scholar?start={0}&hl=en&as_sdt=0,5&q={1}'.format(i, payload)) #, proxies={"http": p, "https": p})
-		html = browser.page_source
-		page = bs(html, "html.parser")
-		data = clean.data(page)
-		tmp = pd.DataFrame(data, columns=['title', 'publication date', 'journal', 'DOI', 'author', 'abstract'])
-		df.append(tmp)
-		time.sleep(1)
-	return (df)
-
-# create empty df
-## concat line per line data from selenium_search
-def create_gs_df(data):
-	df = pandas_utils.df_empty_creator()
-	for d in data:
-		df = pd.concat([df, d], ignore_index=True)
-	return (df)
+def create_gs_df(keywords, browser, limit):
+    payload = (keywords).replace(' ', '+') # change parameters keyword in url form
+    df = pandas_utils.df_empty_creator() # create the empty master df
+    data = [] # creates a list of data
+    for i in range(0, limit, 10): # search results according to a given limit, ecx. limit = 50 results,
+        browser.get('https://scholar.google.com/scholar?start={0}&hl=en&as_sdt=0,5&q={1}'.format(i, payload)) # get pages to scrape
+        html = bs(browser.page_source, "html.parser") # get html
+        page = clean.data(html) # clean html and returns a zipped list ordered by 'title', 'publication date', 'journal', 'DOI', 'author', 'abstract'
+        data.append(pd.DataFrame(page, columns=['title', 'publication date', 'journal', 'DOI', 'author', 'abstract'])) # append data in df form to data list
+        time.sleep(1) # get some rest before scraping again
+    for d in data: # fill in the master df with data
+    	df = pd.concat([df, d], ignore_index=True)
+    return (df)
 
 # main function, call above and return full df
 def gs_df_feeder(keywords):
     # change code to retrieve less than 10 results
     limit = 10 #limit max tested 3000
-    browser = selenium_utils.get_browser()
-    data = s.selenium_search(keywords, browser, limit=limit)
-    df = s.create_gs_df(data)
-
+    browser = selenium_utils.get_browser() # get selenium browser
+    df = create_gs_df(keywords, browser, limit=limit) # scrape data and return df
     # fill database column
     df['from_database']= 'google_scholar'
     # apply date time format to publication date column
     df['publication date']= pd.to_datetime(df['publication date'],yearfirst='True') #ValueError: month must be in 1..12
+    df.to_csv("newdf.csv")
     return(df)
