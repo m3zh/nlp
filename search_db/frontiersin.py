4c5683cf57c61b854e00c70d211f710b506d653f
y3zh <mil3zh@protonmail.com>
2021-04-03 20:41:13 +0200

    add scroll to selenium

--- search_db/frontiersin.py
+++ search_db/frontiersin.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+# import org.openqa.selenium.JavascriptExecutor;
 from bs4 import BeautifulSoup as bs
 import requests
 import sys # needed for import pandas_utils from parent folder
@@ -17,39 +18,44 @@ def frontiersin_df_feeder(keywords):
     browser = get_browser()
     browser.get("https://www.frontiersin.org/search?query={0}&tab=articles".format(keywords))
     time.sleep(10) # replace this line by cookies handling
-    html1 = browser.page_source
-    page1 = bs(html1, "html.parser")
+    for i in range(5): # <-- this is the number times you want the page to load; increase as you like (1 load = 25 results)
+        elem = "#article-results > ul > li:last-child"
+        search= "document.querySelector(\'" + elem + "\').scrollIntoView();"
+        browser.execute_script(search)
+        time.sleep(3) # <-- this is the time the page needs to load the new results; encrease the time if needed/no results are retrieved/your connection is slow
 
-# Start scraping by identifing links
-    test2 = page1.find_all("a", href=True)
-    list = []
-    for t in test2:
-        if 'altmetric' in t['href']:
-            list.append(t['href'])
+    js_script = '''\
+    var s = []
+    document.querySelectorAll('script').forEach(script => s.push(script.src));
+    return s;
+    '''
+    scripts = browser.execute_script(js_script)
+    print(scripts[:3])
+    url_list = []
+    for s in scripts:
+        if 'altmetric' in s:
+            url_list.append(s)
 
-# print each url founded in search page
-    # for url in list:
-        # print(url)
-
-# Metadata to df
-    for count, url in enumerate(list):
-        page2 = browser.get(url)
-        html2 = browser.page_source # retrieve the page source of the webpage browser is accessing
-        page2 = bs(html2, "html.parser")
-        try:
-            df.at[count, 'title'] = page2.find('td').findNext("div").get_text()
-            df.at[count, 'DOI'] = page2.find('td').findNext("a")['href']
-            df.at[count, 'abstract'] = page2.find("td").findNext("div").findNext("p").findNext("span").get_text() # not working for full text
-            # df.at[count, 'abstract'] += page2.find("td").findNext("div").findNext("p").findNext("span").findNext("span").get_text()
-            if df.at[count, 'abstract'] == "##":
-                df.at[count, 'abstract'] = ""
-            df.at[count, 'publication date'] = page2.find("tbody").findNext("tr").findNext("tr").findNext("td").findNext("div").get_text()
-            df.at[count, 'publication date'].split(',') # keep date only year
-            # add reste to journal
-        except AttributeError:
-            pass
-    df['from_database']= 'frontiersin'
-    df.to_csv("./excels/dfFRONTIERSTEST{}.csv".format(datetime.now().time()))
-    # df['publication date']= pd.to_datetime(df['publication date'])
-    browser.close()
+        # Metadata to df
+    # for count, url in enumerate(url_list):
+    #     page2 = browser.get(url)
+    #     html2 = browser.page_source # retrieve the page source of the webpage browser is accessing
+    #     page2 = bs(html2, "html.parser")
+    #     try:
+    #         df.at[count, 'title'] = page2.find('td').findNext("div").get_text()
+    #         df.at[count, 'DOI'] = page2.find('td').findNext("a")['href']
+    #         df.at[count, 'abstract'] = page2.find("td").findNext("div").findNext("p").findNext("span").get_text() # not working for full text
+    #         # df.at[count, 'abstract'] += page2.find("td").findNext("div").findNext("p").findNext("span").findNext("span").get_text()
+    #         if df.at[count, 'abstract'] == "##":
+    #             df.at[count, 'abstract'] = ""
+    #         df.at[count, 'publication date'] = page2.find("tbody").findNext("tr").findNext("tr").findNext("td").findNext("div").get_text()
+    #         df.at[count, 'publication date'].split(',') # keep date only year
+    #         # add reste to journal
+    #     except AttributeError:
+    #         pass
+    #
+    # df['from_database'] = 'frontiersin'
+    # df.to_csv("./excels/dfFRONTIERSTEST{}.csv".format(datetime.now().time()))
+    # # df['publication date']= pd.to_datetime(df['publication date'])
+    # browser.close()
     return(df)
